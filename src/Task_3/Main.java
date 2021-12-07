3489c6725d7528eebc53dff27031b46cadc838eb
RailyaSizykh <railya.sizykh@gmail.com>
2021-12-07 23:51:09 +0300

    Task 3

---
+++ src/Task_3/Main.java
@@ -0,0 +1,12 @@
+package Task_3;
+
+public class Main {
+    public static void main(String[] args) {
+        int[] list = {1, 2, 3, 4, 5};
+        System.out.println(list[0] + list[2]);
+        int t = list[list.length-1];
+        list[list.length-1] = list[0];
+        list[0] = t;
+        System.out.println(list[0] + list[2]);
+    }
+}
